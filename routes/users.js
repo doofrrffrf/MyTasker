fe9ccc67a4b6c9516300db9473177b999775a8da
doofrrffrf <suto8ph@gmailcom>
2023-02-21 19:17:42 +0530

    project completed

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://localhost/task');
+
+mongoose.connect('mongodb://localhost/task');
+let schema = mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    }
+  }
+)
+module.exports = mongoose.model('tasks', schema);
